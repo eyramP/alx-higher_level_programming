f52272194bd3c80aefff4bf9de4e32adbe355cf8
Eyram Cobblah <ecobblah2@gmail.com>
2023-06-12 01:45:49 +0000

    Corrections for tasks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list) - 1:
-        return None
-    return (mylist[idx])
-
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
