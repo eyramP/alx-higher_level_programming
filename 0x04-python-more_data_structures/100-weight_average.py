e5bfe282313d2dce1218d3e9d7b85d47542f540e
Eyram Cobblah <ecobblah2@gmail.com>
2023-06-16 02:38:02 +0000

    Corrections for task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    s = 0
+    if not my_list:
+        return 0
 
-    for i my_list:
-        s = sum(i)
-    return s / leng(my_list)
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
