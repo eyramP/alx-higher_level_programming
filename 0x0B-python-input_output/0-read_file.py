7bd68588a7427247342209bf7acf5279cb9fbe20
Eyram Cobblah <ecobblah2@gmail.com>
2023-07-11 23:25:52 +0000

    Corrections for tasks 9

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,18 @@
-#!/ussr/bin/python3
+#!/usr/bin/python3
+""" Module that contains a function that reads from a file """
+
+
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
